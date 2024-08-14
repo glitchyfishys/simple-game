9b18e8cb240d95ded96bd16cafcdebcf91057a10
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-08-15 09:55:28 +1200

    Update game.js

--- game.js
+++ game.js
@@ -115,7 +115,7 @@ function updateUI(){
         text += progress() > 4 ? `you have <span style="color: lime">${player.money.dilatedtime.toString()}</span> DT <br>` : "<br>";
         text += progress() > 5 ? `you have <span style="color: red">${player.money.relics.toString()}</span> relics <br>` : "<br>";
         if(player.challenge.challengein != -1){
-            if(player.challenge.challengein = 666){
+            if(player.challenge.challengein == 666){
                 text += "you are currently in dilation, reach 1e1000 IP to gain dilated time";
             }
             else{
